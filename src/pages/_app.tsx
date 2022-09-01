18aee4a213a2d3c817a7ada3b179f89e4d3d55ba
Jrevoredo42 <revoredojf@gmail.com>
2022-09-01 12:25:54 -0300

    foi finalizado a estrutura da aplicação, com as fontes do google adicionadas no arquivo criado  document, que serve como um arquivo global para o html do projeto, alem das estilizaçoes globais usando scss e o tittle do projeto

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 //Importação das propriedades que o componente app pode receber (pra tipagem do typescript)
 import { AppProps } from 'next/app'
 
-import '../styles/home.module.scss';
+import '../styles/global.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />

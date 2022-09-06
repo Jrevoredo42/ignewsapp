c6b1b7c10e90dd0e2c80e8dc4d4bc0b14ad5ef99
Jrevoredo42 <revoredojf@gmail.com>
2022-09-06 11:51:54 -0300

    criação e estilização do componente header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import styles from './styles.module.scss'
+// import  Image from 'next/image'
+
+export function Header(){
+    return(
+        <header className={styles.headerContainer}>
+            <div className={styles.headerContent}>
+                <h1>Ig.News</h1>
+                {/* <img src='/images/Logo.svg' alt='ig.news' /> */}
+                <nav>
+                    <a className={styles.active}>Home</a>
+                    <a className={styles.headerContent}>Posts</a>
+                </nav>
+            </div>
+        </header>
+    )
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,15 @@
 //Importação das propriedades que o componente app pode receber (pra tipagem do typescript)
 import { AppProps } from 'next/app'
-
+import { Header } from '../components/Header';
 import '../styles/global.scss';
-
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return(
+    <>
+    <Header/>
+    <Component {...pageProps} />
+    </>
+    
+  ) 
 }
 
 export default MyApp

0a68343b8ee29c6def8e9d8e23c5c1faa96f7558
Jrevoredo42 <revoredojf@gmail.com>
2022-09-06 17:49:21 -0300

    criação e estilização de pagina home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-
+import styles from './home.module.scss'
 
 export default function Home(){
   return (
@@ -7,8 +7,8 @@ export default function Home(){
     <Head>
     <title>Home | ig.news</title>
   </Head>
-    <main>
-      <section>
+    <main className={styles.contentContainer}>
+      <section className={styles.hero}>
         <span>👏🏾 Seja Bem vindo!</span>
         <h1> Notícias sobre o universo <span>React</span></h1>
         <p>
